69ed78f63aa6c16212e89fbdd31b186030a02307
rudylpzjme <rudylpzjme@gmail.com>
2022-12-30 13:26:35 -0600

    MFLIX-002: improving some UI/UX

--- src/slices/settings.ts
+++ src/slices/settings.ts
@@ -1,9 +1,11 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from '../store'
 
 const initialState = {
   isSideBarVisible: false,
+  selectedGenres: ['All'],
   menuOptions: [
+    'All',
     'Drama',
     'History',
     'Short',
@@ -25,6 +27,23 @@ const initialState = {
   ],
 }
 
+const handleCheckGenre = (prevSelectedGenres: string[], genre: string): string[] => {
+  let selectedGenres = prevSelectedGenres
+  const existingGenreIndex = selectedGenres.indexOf(genre)
+  const allGenresIndex = selectedGenres.indexOf('All')
+
+  if (genre === 'All') return ['All']
+  if (allGenresIndex !== -1) selectedGenres.splice(allGenresIndex, 1)
+
+  if (existingGenreIndex !== -1) {
+    selectedGenres.splice(existingGenreIndex, 1)
+  } else {
+    selectedGenres.push(genre)
+  }
+
+  return selectedGenres
+}
+
 const settings = createSlice({
   name: 'settings',
   initialState,
@@ -35,11 +54,14 @@ const settings = createSlice({
         isSideBarVisible: !state.isSideBarVisible,
       }
     },
+    checkGenre: (state, action: PayloadAction<string>) => {
+      state.selectedGenres = handleCheckGenre(state.selectedGenres, action.payload)
+    }
   }
 })
 
 export const selectSettingsState = (state: RootState) => {
   return state.settings
 }
-export const { toggleSideBarVisibility } = settings.actions
+export const { toggleSideBarVisibility, checkGenre } = settings.actions
 export default settings.reducer
